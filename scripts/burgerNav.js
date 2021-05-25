764c2f73f05293790120c3ec3769a1b94c3da032
Utkirbek Kakhorov <84437062+kautkir@users.noreply.github.com>
2021-05-25 21:17:49 +0500

    Add files via upload

---
+++ scripts/burgerNav.js
@@ -0,0 +1,20 @@
+// selectors and variables
+const burgerBtn = document.querySelector('.burger-btn');
+const linksContainer = document.querySelector('.links');
+
+// functions
+function activateNav() {
+	linksContainer.classList.toggle('activate');
+
+	// checking if it's the home page
+	if (document.title === 'Kakhorov Utkirbek') {
+		document
+			.querySelector('.intro-container')
+			.classList.toggle('burger-active');
+	} else {
+		document.querySelector('main').classList.toggle('burger-active');
+	}
+}
+
+// event listeners
+burgerBtn.addEventListener('click', activateNav);
---
+++ scripts/playlistAnimation.js
@@ -0,0 +1,8 @@
+// selectors and variables
+const playlistBtn = document.querySelector('.playlist-btn');
+const playlistContainer = document.querySelector('.playlist-container');
+
+// event listeners
+playlistBtn.addEventListener('click', () => {
+	playlistContainer.classList.toggle('playlist-active');
+});
